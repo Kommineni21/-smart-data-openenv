c2bb2c3356eea2049c95320ba2fea9c0078b5005
Kommineni21 <purandha2006@gmail.com>
2026-04-08 19:30:57 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -24,3 +24,10 @@ def step(action: Action):
 @app.get("/state")
 def state():
     return env.get_state()
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
