83ddfd266de73db526fedf98ba9403df579a3513
Kommineni21 <purandha2006@gmail.com>
2026-04-11 11:23:46 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,33 +1,26 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from env.environment import DataEnv
+from env.environment import DataEnv, DataAction
 
 app = FastAPI()
 
 env = DataEnv()
 
-class Action(BaseModel):
-    action: str
 
 @app.get("/")
 def root():
     return {"status": "ok"}
 
+
 @app.post("/reset")
 def reset():
     return env.reset()
 
+
 @app.post("/step")
-def step(action: Action):
-    return env.step(action.action)
+def step(action: DataAction):
+    return env.step(action)
+
 
 @app.get("/state")
 def state():
-    return env.get_state()
-def main():
-    import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
-
-
-if __name__ == "__main__":
-    main()
+    return env.state()
